53f92be0fbb290272861ca6572bfaf1109f94f78
m-urena <murenaeusebio@gmail.com>
2025-12-11 11:51:58 -0500

    Update retirement_calculator.py
    maybe works

--- retirement_calculator.py
+++ retirement_calculator.py
@@ -23,7 +23,16 @@ st.set_page_config(
 # --------------------------------------------------
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY,
+    options={
+        "headers": {
+            "apikey": SUPABASE_KEY,
+            "Authorization": f"Bearer {SUPABASE_KEY}"
+        }
+    }
+)
 
 
 # --------------------------------------------------
