392fe96267d79e3b0ac2a64060d0c747c517d796
m-urena <murenaeusebio@gmail.com>
2025-12-11 12:14:17 -0500

    Update retirement_calculator.py

--- retirement_calculator.py
+++ retirement_calculator.py
@@ -21,21 +21,23 @@ st.set_page_config(
 # --------------------------------------------------
 # Supabase Setup
 # --------------------------------------------------
-
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]  # MUST be publishable key
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"] 
 
 supabase: Client = create_client(
     SUPABASE_URL,
     SUPABASE_KEY,
     options={
+        "auth": {
+            "persist_session": False
+        },
         "headers": {
-            "apikey": SUPABASE_KEY,
-            "Authorization": f"Bearer {SUPABASE_KEY}"
+            "Authorization": f"Bearer {SUPABASE_KEY}",
+            "apikey": SUPABASE_KEY
         }
     }
 )
-# --------------------------------------------------
+----------------------------------------
 # Logo
 # --------------------------------------------------
 
